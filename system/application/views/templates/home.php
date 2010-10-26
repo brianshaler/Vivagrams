880ccd9010d5d45ba68e24d9296e63c4bc9f6cbb
Brian Shaler <brian@shaler.name>
2010-10-25 22:18:09 -0700

    home screen

--- system/application/views/templates/home.php
+++ system/application/views/templates/home.php
@@ -1,6 +1,17 @@
-<div class="wrapper wrappercontent">
-<div class="content" style="min-height: 400px;">
+<div id="home" class="wrapper">
+  <div style="height: 400px;">
+	<div id="one">
+		<img src="/public/images/1.png">
+		<p class="CronosPro">Choose and customize your health profile.</p>
+	</div>
+	<div id="two">
+		<img src="/public/images/2.png">
+		<p class="CronosPro">Receive and respond to alerts to build healthy habits.</p>
+	</div>
 
-<?=$this->load->view('forms/register', array(), true)?>
-</div>
+	<div id="three">
+		<img src="/public/images/3.png">
+		<p class="CronosPro">Track your own happiness over time.</p>
+	</div>
+  </div>
 </div>
