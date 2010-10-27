3c30f176af5c0b3e91d1be2482e1c1c6aaa4ed39
Aaron Kavlie <akavlie@gmail.com>
2010-10-27 01:02:35 -0700

    Work in progress on account form.

--- system/application/controllers/profile.php
+++ system/application/controllers/profile.php
@@ -9,27 +9,40 @@ class Profile extends Controller {
 		$this->load->library('FAL_front', 'fal_front');
 		$this->load->helper('vivagrams');
 		$this->load->helper('oauth');
+		$this->load->helper('form');
 	}
 	
 	function index()
 	{
-  	$this->load->model('User_Model', '', TRUE);
-  	$this->load->model('Plan_Model', '', TRUE);
-  	$this->load->model('Gram_Model', '', TRUE);
-  	$this->load->model('Message_Model', '', TRUE);
+        $this->load->model('User_Model', '', TRUE);
+        $this->load->model('Usermodel', '', TRUE);
+        $this->load->model('Userprofile', '', TRUE);
+
+        if (isValidUser())
+        {
+            $user = $this->User_Model->get_user_by_id(getUserProperty('id'));
+        }
+        else
+        {
+            redirect("", "location");
+            return;
+        }
+            
+        if(!empty($_POST))
+        {
+            $data = array('notifications' => $this->input->post('notifications'));
+            $this->Userprofile->updateUserProfile(getUserProperty('id'), $data);
+
+            $data = array('user_name' => $this->input->post('phone'), 
+            'password' => '');
+
+        }
+
+        //var_dump($user);
   	
-  	if (isValidUser())
-  	{
-  	  $user = $this->User_Model->get_user_by_id(getUserProperty('id'));
-	  } else
-	  {
-	    redirect("", "location");
-	    return;
-    }
-      	
-		$this->load->view('templates/header', array("user"=>$user));
-    $this->load->view('user/profile');
-		$this->load->view('templates/footer');
+        $this->load->view('templates/header', array('user' => $user));
+        $this->load->view('user/profile', $user);
+        $this->load->view('templates/footer');
 	}
 }	
 ?>
--- system/application/views/user/profile.php
+++ system/application/views/user/profile.php
@@ -1,7 +1,29 @@
 <div class="wrapper wrappercontent">
 <div class="content">
 <h2 class="CronosProBold">My Account</h2>
-<p>This isn't ready yet..</p>
+
+<?=form_open('/profile', array('class' => 'account'))?>
+<?=form_fieldset('User Info')?>
+<p>
+    <?=form_label('Phone', 'phone')?>
+    <?=form_input('phone', $user_name)?>
+</p>
+<p>
+    <?=form_label('Password', 'password')?>
+    <?=form_password('password')?>
+</p>
+<?=form_fieldset_close()?>
+
+<?=form_fieldset('Notifications')?>
+<p>
+    <?=form_label('Enable Notifications', 'notifications')?>
+    <?=form_checkbox('notifications')?>
+</p>
+<?=form_fieldset_close()?>
+
+<?=form_submit('save', 'Save')?>
+</form>
+
 <div style="height: 160px;"><!-- --></div>
 
 </div>
