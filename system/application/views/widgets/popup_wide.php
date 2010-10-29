f82b44f49c6b255b9f636c4629ea66f5944ad31a
Brian Shaler <brian@shaler.name>
2010-10-29 09:02:43 -0400

    Making dashboard view more awesomer

---
+++ system/application/views/widgets/popup_wide.php
@@ -0,0 +1,5 @@
+<div class="wide_popup<? if (isset($class)) { echo " $class"; } ?>">
+<div class="wide_popup_top"><!-- --></div>
+<div class="wide_popup_content"><?=$content?><div style="clear: both; height: 1px;"><!-- --></div></div>
+<div class="wide_popup_bottom"><!-- --></div>
+</div>
