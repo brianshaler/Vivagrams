49337630d877a950e14facb71e3072ab6691e7d1
Brian Shaler <brian@shaler.name>
2010-10-29 00:07:36 -0700

    vivagrams number function

--- system/application/helpers/vivagrams_helper.php
+++ system/application/helpers/vivagrams_helper.php
@@ -1,5 +1,10 @@
 <?php  if (!defined('BASEPATH')) exit('No direct script access allowed');
 
+function vivagrams_number()
+{
+  return "415-555-1234";
+}
+
 $getvars = array();
 function getvar($name, $default = false)
 {
